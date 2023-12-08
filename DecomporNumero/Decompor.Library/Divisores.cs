0fca38e02495d9ca5e7bdc7915f847347dc586d1
GuilhermePereira91 <guihermexmarques91@gmail.com>
2023-12-08 18:43:13 -0300

    Biblioteca Decompor criada

---
+++ DecomporNumero/Decompor.Library/Divisores.cs
@@ -0,0 +1,45 @@
+﻿namespace Decompor.Library
+{
+    public class Divisores
+    {
+        private List<int> _divisoresNaturais = new List<int>();
+        private List<int> _divisoresPrimos = new List<int>();
+
+        public List<int> DivisoresNaturais { get => _divisoresNaturais; }
+        public List<int> DivisoresPrimos { get => _divisoresPrimos; }
+
+        public Divisores(int numero) {
+            _divisoresNaturais = ObterDivisores(numero);
+            _divisoresPrimos = ObterDivisoresPrimos(numero);
+        }
+
+        public List<int> ObterDivisores(int numero)
+        {
+            List<int> divisores = new List<int>();
+
+            for (int i = 0; i <= numero; i++)
+            {
+                if (numero % i == 0)
+                    divisores.Add(i);
+            }
+
+            return divisores;
+        }
+
+        public List<int> ObterDivisoresPrimos(int numero)
+        {
+            List<int> divisores = new List<int>();
+            List<int> primos = new List<int>();
+
+            divisores = ObterDivisores(numero);
+
+            foreach (int i in divisores)
+            {
+                if (ObterDivisores(i).Count == 2)
+                    primos.Add(i);
+            }
+
+            return primos;
+        }
+    }
+}
